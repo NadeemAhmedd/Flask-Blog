28d11ac7edab9209563bda93d0bcf2fd55b83ec9
NadeemAhmedd <aknadeemahmed@gmail.com>
2020-03-06 18:49:00 +0500

    create user model and blogpost model

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,34 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import datetime
 
 app = Flask(__name__)
+
+
+############################
+# DATABASE SETUP ########
+#####################################
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/flask-blog'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#################################################
+
+# LOGIN CONFIGS
+
+#########################
+login_manager = LoginManager()
+
+login_manager.init.app(app)
+login_manager.login_view = 'users.login'
+
+#########################################
 
 from blog.core.views import core
 app.register_blueprint(core)
---
+++ blog/models.py
@@ -0,0 +1,56 @@
+from blog import db, login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+class User(db.Model):
+
+
+    __tablename__ = 'users'
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    profile_image = db.Column(db.String(70), nullable=False, default='default_profile.png')
+    profile_image = db.Column(db.String(70), unique=True, index=True)
+    username = db.Column(db.String(70), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    posts = db.relationship('BlogPost', backref='author', lazy=True)
+
+    def __init__(self, email, username, password):
+        self.email= email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return "Username {self.username}"
+
+
+class BlogPost(db.Model):
+
+    users = db.relationship(User)
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(150), nullable=False)
+    subtitle = db.Column(db.String(150), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+
+    def __init__(self, title, subtitle, text, user_id):
+        self.title = title
+        self.subtitle = subtitle
+        self.text = text
+        self.user_id = user_id
+
+    def __repr__(self):
+        return "Post ID: {self.id} -- Date: {self.date_posted}"
